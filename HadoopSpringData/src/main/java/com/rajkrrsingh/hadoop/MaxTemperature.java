c9eeb89c19eed0d40e1f183ff8f9bf0a78793b74
Rajkumar Singh <rajkrrsingh@gmail.com>
2013-08-28 11:28:26 +0530

    mapper reducer

---
+++ HadoopSpringData/src/main/java/com/rajkrrsingh/hadoop/MaxTemperature.java
@@ -0,0 +1,10 @@
+package com.rajkrrsingh.hadoop;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MaxTemperature {
+
+  public static void main(String[] args) throws Exception {
+	  ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+}
+}
